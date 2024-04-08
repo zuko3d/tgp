f8f76a44b382c275fe1bcfd2482e82e9cc08590c
zuko3d <zuko3d@gmail.com>
2024-04-08 03:49:16 +0300

    Better bot spades

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -9,27 +9,6 @@
 #include <map>
 #include <queue>
 
-constexpr int spadesNeeded__(TerrainType src, TerrainType dst) {
-    int dist = (SC(dst) + 7 - SC(src)) % 7;
-    dist = std::min(dist, 7 - dist);
-    return dist;
-}
-
-constexpr std::array<std::array<int, 7>, 7> spadesNeeded_() {
-    std::array<std::array<int, 7>, 7> ret;
-    for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 7; j++) {
-            ret[i][j] = spadesNeeded__((TerrainType) i, (TerrainType) j);
-        }
-    }
-    return ret;
-}
-
-int spadesNeeded(TerrainType src, TerrainType dst) {
-    constexpr auto r = spadesNeeded_();
-    return r[SC(src)][SC(dst)];
-}
-
 void GameEngine::setLogger(std::function<void(const std::string&)> logger) {
     logger_ = logger;
 }
